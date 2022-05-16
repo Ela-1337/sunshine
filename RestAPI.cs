bdfc4d9c0d2cbb344485119f91be0b0623957c50
Manuela Kalmbach <manuelakalmbach@gmail.com>
2022-05-16 21:30:21 +0200

    API Anfrage Implementierung

--- RestAPI.cs
+++ RestAPI.cs
@@ -1,14 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
 using System.Threading.Tasks;
+
+// NuGet Paket um JSON Format zu verarbeiten
+using Newtonsoft.Json;
+
 
 namespace Bietigheim_Sunshine
 {
-    internal class RestAPI
+    public class RestAPI
     {
-        public string abfrageAPI(string);
+        //http://api.weatherapi.com/v1/current.json?key=068148c7ca61436d91b182327221605&q=Italy&aqi=no
 
+        public RestAPI(string url)
+        {
+            this.url = url + "?key=" + API_KEY;
+        }
+
+
+        //API Anfrage - async, weil Server nicht sofort antworten kann
+        //Task: hält den Zustand der Anfrage (vorläufiges Resultat, man kann den Zustand der Anfrage erfragen)
+        public async Task<WeatherResponse> getWeatherbyCountry(string country)
+        {
+            // Warten (await) solange bis das endültige Resultat der Anfrage vorliegt -> synchroner Ablauf
+            var result = await client.GetStringAsync(this.url + "&q=" + country + "&api=no"); // gibt Task<string> zurück
+
+            // result ist ein string im JSON Format -> Ziel ist es daraus ein C# Objekt zu generieren
+            // NewtonSoft JSON Paket ermöglicht diese Konvertierung
+            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(result);
+
+            return weatherResponse;
+        }
+
+        private string url;
+        private const string API_KEY = "068148c7ca61436d91b182327221605";
+
+        // Erzeugen des .NET HTTPClients (wird nur 1x pro Prozess erzeugt)
+        static readonly HttpClient client = new HttpClient();
     }
 }
---
+++ WeatherResponse.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bietigheim_Sunshine
+{
+    public class Condition
+    {
+        public string text { get; set; }
+        public string icon { get; set; }
+        public int code { get; set; }
+    }
+
+    public class Location
+    {
+        public string name { get; set; }
+        public string region { get; set; }
+        public string country { get; set; }
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public string tz_id { get; set; }
+        public long localtime_epoch { get; set; }
+        public string localtime { get; set; }
+    }
+
+    public class Current
+    {
+        public long last_updated_epoch { get; set; }
+        public string last_updated { get; set; }
+        public double temp_c { get; set; }
+        public double temp_f { get; set; }
+        public int is_day { get; set; }
+        public Condition condition { get; set; }
+        public double wind_mph { get; set; }
+        public double wind_kph { get; set; }
+        public int wind_degree { get; set; }
+        public string wind_dir { get; set; }
+        public double pressure_mb { get; set; }
+        public double pressure_in { get; set; }
+        public double precip_mm { get; set; }
+        public double precip_in { get; set; }
+        public int humidity { get; set; }
+        public int cloud { get; set; }
+        public double feelslike_c { get; set; }
+        public double feelslike_f { get; set; }
+        public double vis_km { get; set; }
+        public double vis_miles { get; set; }
+        public double uv { get; set; }
+        public double gust_mph { get; set; }
+        public double gust_kph { get; set; }
+    }
+
+    public class WeatherResponse
+    {
+        public Location location { get; set; }
+        public Current current { get; set; }
+
+    }
+}
